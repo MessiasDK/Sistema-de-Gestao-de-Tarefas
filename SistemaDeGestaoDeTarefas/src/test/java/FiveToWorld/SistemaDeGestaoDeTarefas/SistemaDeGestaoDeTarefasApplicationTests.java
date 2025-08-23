9b1ddf066957275c381df5abc68cb6137c3766e0
MessiasDK <119983078+MessiasDK@users.noreply.github.com>
2025-08-23 14:02:33 +0200

    unpacking spring project

---
+++ SistemaDeGestaoDeTarefas/src/test/java/FiveToWorld/SistemaDeGestaoDeTarefas/SistemaDeGestaoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package FiveToWorld.SistemaDeGestaoDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
